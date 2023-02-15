7ebdbfc9ab3b277f48fd3d3b7a181d1cd1ee47d3
Rodrigo <rodrigobraga0125@gmail.com>
2023-02-15 18:51:32 -0300

    fix findAll

--- src/main/java/com/rrsys/productsapi/services/ProductService.java
+++ src/main/java/com/rrsys/productsapi/services/ProductService.java
@@ -3,12 +3,7 @@ package com.rrsys.productsapi.services;
 import com.rrsys.productsapi.models.ProductsEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
-
-<<<<<<< HEAD
-import java.awt.print.Pageable;
-=======
 import org.springframework.data.domain.Pageable;
->>>>>>> eca4936 (Create findAll)
 import java.util.UUID;
 
 
--- src/main/java/com/rrsys/productsapi/services/impl/ProductServiceImpl.java
+++ src/main/java/com/rrsys/productsapi/services/impl/ProductServiceImpl.java
@@ -4,15 +4,9 @@ import com.rrsys.productsapi.models.ProductsEntity;
 import com.rrsys.productsapi.repositories.ProductRepository;
 import com.rrsys.productsapi.services.ProductService;
 import org.springframework.data.domain.Page;
-<<<<<<< HEAD
 import org.springframework.stereotype.Service;
-
-import java.awt.print.Pageable;
-=======
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
->>>>>>> eca4936 (Create findAll)
 import java.util.UUID;
 
 @Service
@@ -28,16 +22,10 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Page<ProductsEntity> getAll(Pageable pageable) {
-<<<<<<< HEAD
-        return null;
-    }
-
-=======
         return repository.findAll(pageable);
     }
 
 
->>>>>>> eca4936 (Create findAll)
     @Override
     public ProductsEntity get(UUID id) {
         return repository.findById(id).get();
